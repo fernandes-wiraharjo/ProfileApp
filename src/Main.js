14b2c9b7d4714034016e4d09f168a81e55efbc65
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2018-12-04 20:55:08 +0700

    commit progress get name from inputFormName

--- src/Main.js
+++ src/Main.js
@@ -9,51 +9,51 @@ import {
 
 class Main extends Component {
   render() {
+    const { userName } = this.props;
     return (
       <View style={Styles.classContainer}>
-
-
         <View style={Styles.containerPerSection}>
           <View style={Styles.textContainer}>
             <Text style={Styles.text}>Name</Text>
           </View>
-          <View style={Styles.inputContainer}>
-            <Text style={Styles.inputResult}>Name</Text>
-          </View>
+          <TouchableOpacity
+            onPress={() => this.props.navigateToInputFormName()}
+            style={Styles.inputContainer}
+          >
+            <Text style={Styles.inputResult}>{ userName }</Text>
+          </TouchableOpacity>
         </View>
 
         <View style={Styles.containerPerSection}>
           <View style={Styles.textContainer}>
             <Text style={Styles.text}>Gender</Text>
           </View>
-          <View style={Styles.inputContainer}>
+          <TouchableOpacity style={Styles.inputContainer}>
             <Text style={Styles.inputResult}>Gender</Text>
-          </View>
+          </TouchableOpacity>
         </View>
 
         <View style={Styles.containerPerSection}>
           <View style={Styles.textContainer}>
             <Text style={Styles.text}>Phone</Text>
           </View>
-          <View style={Styles.inputContainer}>
+          <TouchableOpacity style={Styles.inputContainer}>
             <Text style={Styles.inputResult}>Phone</Text>
-          </View>
+          </TouchableOpacity>
         </View>
 
         <View style={Styles.containerPerSection}>
           <View style={Styles.textContainer}>
             <Text style={Styles.text}>Address</Text>
           </View>
-          <View style={Styles.inputContainer}>
+          <TouchableOpacity style={Styles.inputContainer}>
             <Text style={Styles.inputResult}>Address</Text>
-          </View>
+          </TouchableOpacity>
         </View>
 
         <TouchableOpacity style={Styles.buttonSubmit}>
           <Text style={Styles.textButton}>Submit</Text>
         </TouchableOpacity>
-
-
       </View>
     );
   }
@@ -61,11 +61,15 @@ class Main extends Component {
 
 const Styles = StyleSheet.create({
   classContainer: { flex: 1, padding: 15 },
-  containerPerSection: { flexDirection: "row" },
+  containerPerSection: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 10
+  },
   textContainer: { width: "30%" },
   text: { fontSize: 20, fontWeight: "bold" },
   inputContainer: { width: "70%", backgroundColor: "#f33a3a" },
-  inputResult: { fontSize: 20 },
+  inputResult: { fontSize: 20, marginLeft: 5 },
   buttonSubmit: {
     backgroundColor: "#d2d2d2",
     borderRadius: 3,
